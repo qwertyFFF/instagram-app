b3863a1443494db477e7dd51680fb18ad6a93760
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-06-16 22:34:57 -0300

    config routes and include images

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { createAppContainer, createStackNavigator } from "react-navigation";
+import Feed from "./pages/Feed";
+import New from "./pages/New";
+
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Feed,
+      New
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitle: "Instagram"
+      }
+    }
+  )
+);
