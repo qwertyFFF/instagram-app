5e91c073061dc759e8b1c78a3a264624315010a3
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-06-21 17:49:56 -0300

    create permissions to camera and upload files

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,7 @@ export default createAppContainer(
       New
     },
     {
+      initialRouteName: "New",
       defaultNavigationOptions: {
         headerTintColor: "#000",
         headerTitle: <Image style={{ marginHorizontal: 130 }} source={logo} />,
